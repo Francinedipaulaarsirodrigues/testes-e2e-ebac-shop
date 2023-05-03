5a15ccd09d940bf22a7a5515ed9aa98b15322bec
Francine Rodrigues <francinedipaula16@hotmail.com>
2023-05-03 17:28:42 -0300

    Auteraçoes

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+var faker = require( 'faker');
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,12 +11,40 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.get('[class="product-block grid"]')
+        //.first()
+        //.last()
+        //.eq(3)
+        //todo
+        .contains('Abominable Hoodie')
+        .click()
+
+    });
+
+    it.only('Deve adicionar 4 itens ao carrinho', () => {
+        var quantidade = 4
+        cy.get('[class="product-block grid"]')
+        .contains('Abominable Hoodie').click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain',quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade +' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        
+
+
+});
+        
+
     });
 
 
-})
