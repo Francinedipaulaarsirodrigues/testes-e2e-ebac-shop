2ef748192d443c286dacee4268f8bd4fb035269f
Francine Rodrigues <francinedipaula16@hotmail.com>
2023-05-08 23:05:54 -0300

    Atualização cadastro de faturamento

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,9 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Deve fazer checkout com sucesso.', () => {
+import EndereçoPage from "../support/page_objects/Endereço.page";
+
+describe('Funcionalidade endereco -Faturamento e entrega.', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados =>{
+            cy.login(dados.usuario,dados.senha)
+        })
     });
     
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EndereçoPage.editarEnderecoFaturamento('Flavio', 'Araujo', 'Ebac', 'Brasil','av Brasil',' 3100', 'São Paulo', 'São Paulo', '01000100',',11989898998','flavio@templateSettings.com')
+        cy.get('.woocommerce-message').shoud('contain', 'Endereço alterado com sucesso.')
+
+    });
 });
