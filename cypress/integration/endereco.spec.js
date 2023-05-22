a130ab8a1e4205b22e4cf3b23f6905b998083ca3
Francine Rodrigues <francinedipaula16@hotmail.com>
2023-05-22 16:14:52 -0300

    modificado

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import EndereçoPage from "../support/page_objects/Endereço.page";
+cont dadosEndereco = require( '../fixtures/endereco.json')
 
 describe('Funcionalidade endereco -Faturamento e entrega.', () => {
 
@@ -13,6 +14,25 @@ describe('Funcionalidade endereco -Faturamento e entrega.', () => {
     
     it('Deve fazer cadastro de faturamento com sucesso', () => {
         EndereçoPage.editarEnderecoFaturamento('Flavio', 'Araujo', 'Ebac', 'Brasil','av Brasil',' 3100', 'São Paulo', 'São Paulo', '01000100',',11989898998','flavio@templateSettings.com')
+        cy.get('.woocommerce-message').shoud('contain', 'Endereço alterado com sucesso.')
+
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados.', () => {
+        EndereçoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+
+            )
         cy.get('.woocommerce-message').shoud('contain', 'Endereço alterado com sucesso.')
 
     });
